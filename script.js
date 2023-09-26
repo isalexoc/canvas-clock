054ec13374b55263644721a3cd8388b00b1a6d06
Isaac Orraiz <isaacorraiz@gmail.com>
2023-09-26 15:43:58 -0400

    Clock Draw finished

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function clock() {
   ctx = canvas.getContext("2d");
 
   //Setup canvas
-  ctx.save; //save default state
+  ctx.save(); //save default state
   ctx.clearRect(0, 0, 500, 500);
   ctx.translate(250, 250); // Put 0,0 in the middle
   ctx.rotate(-Math.PI / 2); //Rotate clock -90 deg
@@ -52,7 +52,56 @@ function clock() {
 
   ctx.restore();
 
-  ctx.restore; // restore the default state
+  // Get current time
+  const hr = now.getHours() % 12;
+  const min = now.getMinutes();
+  const sec = now.getSeconds();
+
+  //Draw hour hand
+
+  ctx.save();
+  ctx.rotate(
+    (Math.PI / 6) * hr + (Math.PI / 360) * min + (Math.PI / 21600) * sec
+  );
+  ctx.strokeStyle = "#800000";
+  ctx.lineWidth = 7;
+  ctx.beginPath();
+  ctx.moveTo(-20, 0);
+  ctx.lineTo(80, 0);
+  ctx.stroke();
+  ctx.restore();
+
+  //Draw minutes hand
+
+  ctx.save();
+  ctx.rotate((Math.PI / 30) * min + (Math.PI / 1800) * sec);
+  ctx.strokeStyle = "#800000";
+  ctx.lineWidth = 4;
+  ctx.beginPath();
+  ctx.moveTo(-28, 0);
+  ctx.lineTo(112, 0);
+  ctx.stroke();
+  ctx.restore();
+
+  //Draw seconds hand
+
+  ctx.save();
+  ctx.rotate((Math.PI / 30) * sec);
+  ctx.strokeStyle = "#ff7f50";
+  ctx.fillStyle = "#FF7F50";
+  ctx.lineWidth = 4;
+  ctx.beginPath();
+  ctx.moveTo(-28, 0);
+  ctx.lineTo(112, 0);
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.arc(0, 0, 10, 0, Math.PI * 2, true);
+  ctx.fill();
+  ctx.restore();
+
+  ctx.restore(); // restore the default state
+
+  requestAnimationFrame(clock);
 }
 
-clock();
+requestAnimationFrame(clock);
